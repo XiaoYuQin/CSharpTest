0ef13ffe8f07803ab3664c64d97cdf09bf27dee2
秦晓宇 <qinxiaoyu@163.com>
2016-05-23 17:33:19 +0800

    添加了c#读写SD卡扇区的项目，目前已经实现了SD卡扇区读写

---
+++ SDcard/SDcard/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDcard
+{
+    public partial class Form1 : Form
+    {
+        SDUtils SDutils = new SDUtils("N:");
+        byte[] SectorBytes;
+        private System.Timers.Timer timer = new System.Timers.Timer();
+        long time;
+        public Form1()
+        {
+            InitializeComponent();
+            SectorBytes = new byte[512];
+
+            Console.WriteLine("Form1");
+            Console.WriteLine("SectorLength = " + SDutils.SectorLength);
+
+            for (int i = 0; i < 512; i++)
+            {
+                SectorBytes[i] = (byte)('1'+i);            
+            }
+            SDutils.WriteSector(SectorBytes, 1);
+
+            byte[] readSectorBytes = new byte[512];
+            readSectorBytes = SDutils.ReadSector(1);
+            string str1 = System.Text.Encoding.Default.GetString(readSectorBytes);
+            textBox1.Text = str1;
+            Console.WriteLine(str1);
+
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Enabled = true;
+
+
+            
+            //1000000*12.3#16.8!\r\n
+
+            /*for (int i = 0; i < 512; i++)
+            {
+                Console.Write(readSectorBytes[i]);
+                Console.Write(' ');
+            }
+            Console.WriteLine("");*/
+
+            /*readSectorBytes = SDutils.ReadSector(2);
+            string str2 = System.Text.Encoding.Default.GetString(readSectorBytes);
+            Console.WriteLine(str2);*/
+            /*for (int i = 0; i < 512; i++)
+            {
+                Console.Write(readSectorBytes[i]);
+                Console.Write(' ');
+            }
+            Console.WriteLine("");*/
+
+
+
+
+        }
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //模拟的做一些耗时的操作
+            System.Threading.Thread.Sleep(2000);
+            time++;
+
+        }
+
+
+    }
+}
---
+++ SDcard/SDcard/Profile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace SDcard
+{
+    class Profile
+    {
+        public static void LoadProfile()
+        {
+            string strPath = AppDomain.CurrentDomain.BaseDirectory;
+            _file = new IniFile(strPath + "Cfg.ini");
+            G_PictureNumbers = _file.ReadString("CONFIG", "PictureNumbers", "0");
+            G_StartSector = _file.ReadString("CONFIG", "StartSector", "0");
+            G_SectorNumbers = _file.ReadString("CONFIG", "SectorNumbers", "0");
+            G_PictureHeight = _file.ReadString("CONFIG", "PictureHeight", "0");
+            G_PictureWidth = _file.ReadString("CONFIG",  "PictureWidth", "0");
+        }
+        public static void SaveProfile()
+        {
+            string strPath = AppDomain.CurrentDomain.BaseDirectory;
+            _file.WriteString("CONFIG", "PictureNumbers", G_PictureNumbers);            //写数据，下同
+            _file.WriteString("CONFIG", "StartSector",    G_StartSector);
+            _file.WriteString("CONFIG", "SectorNumbers",  G_SectorNumbers);
+            _file.WriteString("CONFIG", "PictureHeight",  G_PictureHeight);
+            _file.WriteString("CONFIG", "PictureWidth",   G_PictureWidth);
+        }
+	
+        private static IniFile _file;//内置了一个对象
+        public static string G_PictureNumbers = "0";
+        public static string G_StartSector = "0";
+        public static string G_SectorNumbers = "0";
+        public static string G_PictureHeight = "0";
+        public static string G_PictureWidth = "0";
+    }
+}
