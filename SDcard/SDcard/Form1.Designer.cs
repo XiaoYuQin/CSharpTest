94009731b90fb49a8e7f38a7cb3fe3a578551800
秦晓宇 Lenovo yoga213 <qinxiaoyu@163.com>
2016-06-02 00:04:27 +0800

    读写SD卡扇区完成

--- SDcard/SDcard/Form1.Designer.cs
+++ SDcard/SDcard/Form1.Designer.cs
@@ -31,41 +31,76 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 512);
+            this.button1.Location = new System.Drawing.Point(14, 604);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 27);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "上一页";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(123, 512);
+            this.button2.Location = new System.Drawing.Point(121, 604);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(86, 27);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "下一页";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(16, 49);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.textBox1.Size = new System.Drawing.Size(392, 476);
+            this.textBox1.Size = new System.Drawing.Size(447, 549);
             this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "盘符";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(214, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "打开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(416, 557);
+            this.ClientSize = new System.Drawing.Size(475, 643);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -81,6 +116,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- SDcard/SDcard/Form1.cs
+++ SDcard/SDcard/Form1.cs
@@ -7,35 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using System.IO;
 
 namespace SDcard
 {
     public partial class Form1 : Form
     {
-        SDUtils SDutils = new SDUtils("N:");
+        SDUtils SDutils;
         byte[] SectorBytes;
         private System.Timers.Timer timer = new System.Timers.Timer();
         long time;
+        long index = 1;
         public Form1()
         {
             InitializeComponent();
             SectorBytes = new byte[512];
 
-            Console.WriteLine("Form1");
-            Console.WriteLine("SectorLength = " + SDutils.SectorLength);
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
 
-            for (int i = 0; i < 512; i++)
+            foreach (DriveInfo d in allDrives)
             {
-                SectorBytes[i] = (byte)('1'+i);            
+                Console.WriteLine("Drive {0}", d.Name);
+                Console.WriteLine("  File type: {0}", d.DriveType);
             }
-            SDutils.WriteSector(SectorBytes, 1);
 
-            byte[] readSectorBytes = new byte[512];
-            readSectorBytes = SDutils.ReadSector(1);
-            string str1 = System.Text.Encoding.Default.GetString(readSectorBytes);
-            textBox1.Text = str1;
-            Console.WriteLine(str1);
 
+            Console.WriteLine("---------------------");
+
+            foreach (DriveInfo d in allDrives)
+            {
+                if (d.DriveType == DriveType.Removable)
+                {
+                    Console.WriteLine("Drive {0}", d.Name);
+                    comboBox1.Items.Add(d.Name);
+                    comboBox1.SelectedIndex = 0;
+                }
+            }
+
+
+         
             timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
             timer.Enabled = true;
 
@@ -71,6 +82,61 @@ namespace SDcard
             time++;
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SDutils = new SDUtils("E:");
+            Console.WriteLine("Form1");
+            Console.WriteLine("SectorLength = " + SDutils.SectorLength);
+
+           /* for (int i = 0; i < 512; i++)
+            {
+                SectorBytes[i] = (byte)('1' + i);
+            }
+            SDutils.WriteSector(SectorBytes, 1);*/
+
+            byte[] readSectorBytes = new byte[512];
+            readSectorBytes = SDutils.ReadSector(1);
+            string str1 = System.Text.Encoding.Default.GetString(readSectorBytes);
+            string s1 = str1.Insert(41, "\r\n");
+            string s2 = s1.Insert(84, "\r\n");
+            textBox1.Text = s2;
+            Console.WriteLine(s2);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            index--;
+            if (index > 0)
+            {
+                Console.WriteLine("index = " + index);
+                byte[] readSectorBytes = new byte[512];
+                readSectorBytes = SDutils.ReadSector(index);
+                string str1 = System.Text.Encoding.Default.GetString(readSectorBytes);
+                string s1 = str1.Insert(41, "\r\n");
+                string s2 = s1.Insert(84, "\r\n");
+                textBox1.Text = s2;
+                Console.WriteLine(s2);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            index++;
+            if (index < 99999)
+            {
+                Console.WriteLine("index = " + index);
+                byte[] readSectorBytes = new byte[512];
+                readSectorBytes = SDutils.ReadSector(index);
+                string str1 = System.Text.Encoding.Default.GetString(readSectorBytes);
+                string s1 = str1.Insert(41, "\r\n");
+                string s2 = s1.Insert(84, "\r\n");
+                textBox1.Text = s2;
+                Console.WriteLine(s2);
+            }
+        }
+
 
 
     }
